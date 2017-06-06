b710eb8c78cf0380e7557c684fef61f7f7ffc4c4
Andrew (anoa) <anoa@openmailbox.org>
2017-06-05 19:14:09 -0700

    Initial commit. Context menus done
    Signed-off-by: Andrew (anoa) <anoa@openmailbox.org>

---
+++ doc/documentation.md
@@ -0,0 +1,35 @@
+# Documentation
+
+## File Manager Context Menus
+
+The context menus are defined as a python script for Nautilus
+(`/usr/share/nautilus-python/extensions` and a service file
+(`/usr/share/kde4/services/`) for Dolphin. Both call
+`/usr/lib/qubes/qvm-set-trust.{gnome,kde}` which actually creates the dialog
+asking the user which file trust settings should be set, as well as setting any
+emblems on the file in question.
+
+## File Manager Patches
+
+A patch is needed for each file manager in order to allow extension execution
+upon opening of a file. The extension should be able to determine whether or not
+the file should be opened.
+
+Bug report for Dolphin patch:
+
+Bug report for Nautilus patch:
+
+## File-watcher Daemon
+
+A C daemon will be run in each AppVM that watches and enforces file trust
+settings on any files created inside of folders marked with a specific trust
+level. For example, if I mark a folder as `untrusted`, the daemon should
+automatically mark all new and existing files and folders within as `untrusted`
+as well.
+
+## Unit tests
+
+Unit tests are included in the unit-tests folder.
+
+GUI-based unit tests are done with the
+[Dogtail](https://gitlab.com/dogtail/dogtail) library.
---
+++ qvm_folder_set_trust.py
@@ -0,0 +1,40 @@
+import subprocess
+
+from gi.repository import Nautilus, GObject
+
+
+class FolderSetTrustItemExtension(GObject.GObject, Nautilus.MenuProvider):
+    '''Set trust level for a folder and its contents.
+
+    Uses the nautilus-python api to provide a context menu within Nautilus which
+    enables the user to select trust levels for a folder and its contents.
+    '''
+    def get_file_items(self, window, files):
+        # Attaches context menu in Nautilus
+        if not files:
+            return
+
+        for file_obj in files:
+            # Do not attach context menu to anything other than a local file
+            #if file_obj.get_uri_scheme() != 'file':
+            #    return
+
+            # Only show for folders
+            if not file_obj.is_directory():
+                return;
+
+        menu_item = Nautilus.MenuItem(name='QubesMenuProvider::FolderSetTrust',
+                                      label='Always open contents in DisposableVM',
+                                      tip='',
+                                      icon='checkmark')
+
+        menu_item.connect('activate', self.on_menu_item_clicked, files)
+        return menu_item,
+
+    def on_menu_item_clicked(self, menu, files):
+        '''Called when user chooses files though Nautilus context menu.
+        '''
+        for file_obj in files:
+            # Check if folder still exists
+            if file_obj.is_gone():
+                return
